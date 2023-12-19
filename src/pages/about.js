01653d2171a0733814d02683c40bdc8f2a02783c
Your Name <you@example.com>
2023-12-19 11:17:43 -0300

    Finished Gatsby Tutorial Part 3

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,17 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const AboutPage = () => {
   return (
     <Layout pageTitle='About Me'>
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <StaticImage
+        alt="Minha imagem em um restaurante"
+        src="../images/foto-perfil.jpg"
+      />
     </Layout>
   )
 }
